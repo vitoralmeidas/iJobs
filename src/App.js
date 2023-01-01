470de56572c445b62a1bb8ffb968f11117c803b1
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-01 19:29:15 -0300

    Logo component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Landing from "./pages/Landing";
 function App() {
   return (
     <div>
-      App
       <Landing />
     </div>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,9 +1,30 @@
 import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import main from '../assets/images/main-alternative.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+
 
 const Landing = () => {
     return (
-        <h1>Landing</h1>
+        <Wrapper>
+            <nav>
+                <img src={logo} alt='job' className='logo' />
+            </nav>
+            <div className='container page'>
+                <div className='info'>
+                    <h1>
+                        Job <span>Tracking</span> App
+                    </h1>
+                    <p>
+                        I'm baby tofu Brooklyn sriracha microdosing scenester locavore.
+                        Cronut art party literally kogi cray, fam copper mug butcher YOLO
+                        selfies.
+                    </p>
+                    <button className='btn btn-hero'>Login/Register</button>
+                </div>
+                <img src={main} alt='job hunt' className='img main-img' />
+            </div>
+        </Wrapper>
     )
 }
+
 export default Landing
