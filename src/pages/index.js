025c7eb1974c14fd2a4199896786cb5776988aef
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-05 21:37:21 -0300

    page structure

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from "./Dashboard";
+import Register from "./Register";
+import Error from "./Error";
+import Landing from "./Landing";
+
+export { Dashboard, Register, Error, Landing }
