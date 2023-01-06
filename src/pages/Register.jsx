3ab4b66b843c58f25a8d1d07cef01114e4d27b80
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-05 22:12:55 -0300

    Creating a new component do grab form logic ...

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,19 +2,45 @@ import { useState, useEffect } from "react"
 import { Logo } from "../components"
 import Wrapper from "../assets/wrappers/RegisterPage"
 
+const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    isMember: true,
+}
+
 const Register = () => {
 
-    const initialState = {
-        name: "",
-        email: "",
-        password: "",
-        isMember: true,
+    const [values, setValues] = useState(initialState)
+
+    const handleChange = (e) => {
+        console.log(e.target)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target)
     }
 
   return (
-    <h1>
-      Register
-    </h1>
+    <Wrapper className='full-page'>
+        <form className="form" onSubmit={onSubmit}>
+            <Logo  />
+            <h3>Login</h3>
+
+            <div className="form-row">
+                <label className="form-label" htmlFor="name">Name</label>
+                <input 
+                    type='text'
+                    value={values.name}
+                    onChange={handleChange}
+                    className='form-input'
+                />
+
+            <button type='submit' className="btn btn-block">Submit</button>
+            </div>
+        </form>
+    </Wrapper>
   )
 }
 
