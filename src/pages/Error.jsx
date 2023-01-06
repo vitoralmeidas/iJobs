0feb637a3980ea8e42dcc9e38058c92871f2a87b
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-05 21:59:44 -0300

    Register page v1..

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom"
+import Wrapper from "../assets/wrappers/ErrorPage"
+import img from '../assets/images/not-found.svg'
+
 const Error = () => {
   return (
-   <h1>
-    Error
-   </h1>
+    <Wrapper className="full-page">
+
+        <div>
+
+            <img src={img} alt='not found' />
+            <h3>Ops! Page Not Found</h3>
+            <p>We can't seem to find the page you're looking for...</p>
+            <Link to='/'>Back Home</Link>
+        </div>
+    </Wrapper>
   )
 }
 export default Error
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,16 @@
+import { useState, useEffect } from "react"
+import { Logo } from "../components"
+import Wrapper from "../assets/wrappers/RegisterPage"
+
 const Register = () => {
+
+    const initialState = {
+        name: "",
+        email: "",
+        password: "",
+        isMember: true,
+    }
+
   return (
     <h1>
       Register
