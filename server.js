5a13e6517d644a30fa0858430b29f326412efd89
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-06 22:22:03 -0300

    connectDB ok ---> checking ~DeprecatuibWarning: Mongoose~

--- server.js
+++ server.js
@@ -3,22 +3,34 @@ import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
 
-// middleware
+import connectDB from './db/connect.js'
+
 import errorHandlerMiddleware from './middleware/error-handler.js'
 import notFoundMiddleware from './middleware/not-found.js'
 
 app.get("/", (req, res) => {
     // the error handler middleware will get this error 
-    throw new Error('error')
+    // throw new Error('error')
+
     res.send("Let'sss go")
 })
 
 // looking for requests
 app.use(notFoundMiddleware)
+// looking for general errors
 app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}...`)
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
