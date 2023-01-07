39d5798552f3defa3224a8fafffbfdaac2f2f495
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-06 21:27:34 -0300

    fixing the branchs

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express'
+const app = express()
+
+// middleware
+import errorHandlerMiddleware from './middleware/error-handler.js'
+import notFoundMiddleware from './middleware/not-found.js'
+
+app.get("/", (req, res) => {
+    throw new Error('error')
+    res.send("Let'sss go")
+})
+
+// looking for requests
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`)
+})
