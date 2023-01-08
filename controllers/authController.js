cd53f7c02ad8c262c37aba90f27f13343d408a83
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-08 17:50:05 -0300

    .

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,36 +3,34 @@ import { StatusCodes } from 'http-status-codes'
 import { BadRequestError } from '../errors/index.js'
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body
+  const { name, email, password } = req.body
 
-    if (!name || !email || !password) {
-        throw new BadRequestError('please provide all values')
-    }
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values')
+  }
 
-    // // duplicate email
-    const userAlreadyExists = await User.findOne({ email })
-    if (userAlreadyExists) throw new BadRequestError('Email already in use')
+  // // duplicate email
+  const userAlreadyExists = await User.findOne({ email })
+  if (userAlreadyExists) throw new BadRequestError('Email already in use')
 
-    const user = await User.create({ name, email, password })
-    const token = user.createJWT()
-    res
-        .status(StatusCodes.CREATED)
-        .json({
-            user: {
-                email: user.email,
-                lastName: user.lastName,
-                location: user.location,
-                name: user.name
-            },
-            token
-        })
+  const user = await User.create({ name, email, password })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name
+    },
+    token
+  })
 }
 
 const login = async (req, res) => {
-    res.send('Login User')
+  res.send('Login User')
 }
 const updateUser = async (req, res) => {
-    res.send('Update User')
+  res.send('Update User')
 }
 
 export { register, login, updateUser }
