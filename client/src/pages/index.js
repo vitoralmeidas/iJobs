abff4fdbef352468a853710349797dc3115fa637
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-11 21:56:23 -0300

    Protected Route

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,6 @@
 import Register from './Register'
 import Error from './Error'
 import Landing from './Landing'
+import ProtectedRoute from './ProtectedRoute'
 
-export { Register, Error, Landing }
+export { Register, Error, Landing, ProtectedRoute }
