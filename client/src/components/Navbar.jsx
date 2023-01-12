68dc424c9b151ab5e09981cd1dce360a4504ce7f
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-12 20:46:07 -0300

    Navbar structure

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,38 @@
 import Wrapper from '../assets/wrappers/Navbar'
+import { FaAlignLeft, FaCaretDown, FaUserCircle } from 'react-icons/fa'
+import Logo from './Logo'
 
 const Navbar = () => {
   return (
     <Wrapper>
-      <h1>Navbar</h1>
+      <div className='nav-center'>
+        <button
+          className='toggle-btn'
+          onClick={() => console.log('toogle sidebar')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button className='btn' onClick={() => console.log('show logout')}>
+            <FaUserCircle />
+            Vitor
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              className='dropdown-btn'
+              onClick={() => console.log('logout user')}
+              type='button'
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
