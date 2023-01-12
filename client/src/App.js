31dc1af41764e0982033101ff7e3dd88acbf2c17
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-11 21:21:59 -0300

    Dashboard pages setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Error, Dashboard, Landing, Register } from '../src/pages'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Error, Landing, Register } from './pages'
+import {
+  AddJob,
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats
+} from './pages/dashboard'
 
-function App() {
+function App () {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="*" element={<Error />} />
+        <Route path='/' element={<div>Dashboard</div>} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,5 @@
-import Dashboard from "./Dashboard";
-import Register from "./Register";
-import Error from "./Error";
-import Landing from "./Landing";
+import Register from './Register'
+import Error from './Error'
+import Landing from './Landing'
 
-export { Dashboard, Register, Error, Landing }
+export { Register, Error, Landing }
