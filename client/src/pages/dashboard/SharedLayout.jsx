d94f06f557601636880e78addbc030da0a5493ce
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-11 21:44:09 -0300

    nested routes...

--- client/src/pages/dashboard/SharedLayout.jsx
+++ client/src/pages/dashboard/SharedLayout.jsx
@@ -1,6 +1,15 @@
+import { Outlet, Link } from "react-router-dom"
+import Wrapper from '../../assets/wrappers/SharedLayout'
+
 const SharedLayout = () => {
   return (
-    <div>SharedLayout</div>
+    <Wrapper>
+      <nav>
+        <Link to='add-job'>Add Jobs</Link>
+        <Link to='all-jobs'>All Jobs</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
   )
 }
 export default SharedLayout
