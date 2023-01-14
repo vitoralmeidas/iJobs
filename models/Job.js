45bb5c9dfced8ca1b0133c19d186b9938c237646
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-14 16:48:04 -0300

    Job Model

---
+++ models/Job.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxLength: 50
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxLength: 100
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending'
+    },
+
+    jobType: {
+      type: String,
+      enum: ['full-time', 'part-time', 'remote', 'intership'],
+      default: 'full-time'
+    },
+    jobLocation: {
+      type: String,
+      default: 'my city',
+      required: true
+    },
+
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user']
+    }
+  },
+  // to assing createdAt and updatedAt to your Schema
+  { timestamps: true }
+)
+
+export default mongoose.model('Job', JobSchema)
