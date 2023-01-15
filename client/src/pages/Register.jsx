8806e1235a4dc705a6ecb6994c231478a9a8871f
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-14 23:03:32 -0300

    create the select element on AddJob

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,109 +1,108 @@
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import { Logo, FormRow, Alert } from "../components"
-import Wrapper from "../assets/wrappers/RegisterPage"
-import { useAppContext } from "../context/appContext"
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Logo, FormRow, Alert } from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+import { useAppContext } from '../context/appContext'
 
 const initialState = {
-    name: "",
-    email: "",
-    password: "",
-    isMember: true,
- }
+  name: '',
+  email: '',
+  password: '',
+  isMember: true
+}
 
 const Register = () => {
-    const { isLoading, showAlert, displayAlert, setUpUser, user } = useAppContext()
-    const [values, setValues] = useState(initialState)
-    const navigate = useNavigate()
+  const { isLoading, showAlert, displayAlert, setUpUser, user } =
+    useAppContext()
+  const [values, setValues] = useState(initialState)
+  const navigate = useNavigate()
 
-    const handleChange = (e) => {
-        setValues({...values, [e.target.name]: e.target.value})
+  const handleChange = e => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+
+  useEffect(() => {
+    if (user !== null) {
+      setTimeout(() => {
+        navigate('/')
+      }, 3000)
     }
+  }, [user, navigate])
 
-    useEffect(()=>{
-
-        if (user !== null) {
-            setTimeout(() => {
-                navigate('/')
-            }, 3000)
-        }
-
-    }, [user, navigate])
-
-    const onSubmit = (e) => {
-        e.preventDefault()
-        const {name, email, password, isMember} = values;
-        if (!email || !password || (!isMember && !name )) {
-            displayAlert()
-            return
-        }
-        const currentUser = {name, email , password}
-        if (isMember) {
-            console.log("already a member")
-            setUpUser({
-                currentUser,
-                endPoint: 'login',
-                alertText: 'Login Successful! Redirecting...'
-            })
-        } else {
-            setUpUser({
-                currentUser,
-                endPoint: 'register',
-                alertText: 'User Created! Redirecting...'            
-            })
-        }
+  const onSubmit = e => {
+    e.preventDefault()
+    const { name, email, password, isMember } = values
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert()
+      return
     }
+    const currentUser = { name, email, password }
+    if (isMember) {
+      setUpUser({
+        currentUser,
+        endPoint: 'login',
+        alertText: 'Login Successful! Redirecting...'
+      })
+    } else {
+      setUpUser({
+        currentUser,
+        endPoint: 'register',
+        alertText: 'User Created! Redirecting...'
+      })
+    }
+  }
 
-    const toggleMember = () => {
-        setValues({...values, isMember: !values.isMember})
-    };
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember })
+  }
 
   return (
     <Wrapper className='full-page'>
-        <form className="form" onSubmit={onSubmit}>
-            
-            <Logo  />
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
 
-            <h3>{values.isMember ? "Login": 'Register'}</h3>
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
 
-            {showAlert && <Alert /> }
+        {showAlert && <Alert />}
 
-            {!values.isMember && (
-                <FormRow 
-                    type='text'
-                    value={values.name}
-                    handleChange={handleChange}
-                    name='name'
-                />
-            )}
+        {!values.isMember && (
+          <FormRow
+            type='text'
+            value={values.name}
+            handleChange={handleChange}
+            name='name'
+          />
+        )}
 
-            {/* email input */}
-            <FormRow 
-                name='email'
-                type='text' 
-                handleChange={handleChange} 
-                value={values.email} 
-            />
+        {/* email input */}
+        <FormRow
+          name='email'
+          type='text'
+          handleChange={handleChange}
+          value={values.email}
+        />
 
-            {/* password input */}
-            <FormRow 
-                name='password'
-                type='password' 
-                handleChange={handleChange} 
-                value={values.password} 
-            />
+        {/* password input */}
+        <FormRow
+          name='password'
+          type='password'
+          handleChange={handleChange}
+          value={values.password}
+        />
 
-            <button type='submit' className="btn btn-block" disabled = {isLoading} >Submit</button>
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
+          Submit
+        </button>
 
-            {/* toogle button */}
-            <p>
-                {values.isMember ? 'Not a member yet ?' : 'Already a member'}
+        {/* toogle button */}
+        <p>
+          {values.isMember ? 'Not a member yet ?' : 'Already a member'}
 
-                <button type='button' onClick={toggleMember} className='member-btn'>{
-                    values.isMember? 'Register': 'Login'}
-                </button>
-            </p>
-        </form>
+          <button type='button' onClick={toggleMember} className='member-btn'>
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
     </Wrapper>
   )
 }
--- client/src/pages/dashboard/AddJob.jsx
+++ client/src/pages/dashboard/AddJob.jsx
@@ -1,4 +1,79 @@
+import Wrapper from '../../assets/wrappers/DashboardFormPage'
+import { FormRow, Alert } from '../../components'
+import { useAppContext } from '../../context/appContext'
 const AddJob = () => {
-  return <h1>AddJob</h1>
+  const {
+    isEditing,
+    showAlert,
+    displayAlert,
+    jobLocation,
+    position,
+    company,
+    jopType,
+    jobTypeOptions,
+    status,
+    statusOptions
+  } = useAppContext()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if (!position || !company || !jobLocation) {
+      displayAlert()
+      return
+    }
+    console.log('job created')
+  }
+
+  const handleInputJob = e => {
+    const name = e.target.name
+    const value = e.target.value
+    console.log(`${name}: ${value}`)
+  }
+
+  return (
+    <Wrapper>
+      <form className='form' onSubmit={handleSubmit}>
+        <h3>{isEditing ? 'edit job' : 'add job'}</h3>
+
+        {showAlert && <Alert />}
+
+        <div className='form-center'>
+          <FormRow
+            type='text'
+            name='position'
+            value={position}
+            handleChange={handleInputJob}
+          />
+
+          <FormRow
+            type='text'
+            name='company'
+            value={company}
+            handleChange={handleInputJob}
+          />
+
+          <FormRow
+            labelText='Job Location'
+            type='text'
+            name='jobLocation'
+            value={jobLocation}
+            handleChange={handleInputJob}
+          />
+          {/* select  status*/}
+          {/* select job type */}
+          <div className='btn-container'>
+            <button
+              className='btn btn-block submit-btn'
+              type='submit'
+              disabled=''
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  )
 }
 export default AddJob
