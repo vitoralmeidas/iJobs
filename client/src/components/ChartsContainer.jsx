69e0a6727e504a9cbd1f66c2f0d423bd3cdc264e
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-20 23:00:38 -0300

    starting with BarChart using Recharts,js

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,4 +1,22 @@
+import { useState } from 'react'
+import { useAppContext } from '../context/appContext'
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>
+  const [barChart, setBarChart] = useState(true)
+  const { monthlyApplications: data } = useAppContext()
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applicaitons</h4>
+
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  )
 }
 export default ChartsContainer
