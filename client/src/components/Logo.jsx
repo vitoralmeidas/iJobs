1b1fc5fa2fcfc386eabdcc41431876cab1792e6c
vitoralmeidas <vitor.oliveira1904@gmail.com>
2023-01-22 02:04:20 -0300

    new logo

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -3,9 +3,7 @@ import logo from '../assets/images/logo.svg'
 import React from 'react'
 
 const Logo = () => {
-  return (  
-      <img src={logo} alt='jobtrack' className='logo'/>
-  )
+  return <img src={logo} alt='jobtrack' className='logo' />
 }
 
 export default Logo
